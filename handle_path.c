aad83b49f443501dd4ce198e7251179a11b07f75
Malek Salem <malek.salem.14@gmail.com>
2020-04-14 21:25:42 +0100

    add project files

---
+++ handle_path.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * _getenv - get enviroment variable by name
+ * @name: name of the variable
+ * @env: double pointer array contains env variables
+ * Return: value of variable
+ */
+
+char *_getenv(char *name, char **env)
+{
+
+	char *mem_pt;
+	int ptr_i = 0, idx;
+	int validate = 0;
+	int len_name = 0;
+
+	len_name = _strlen(name);
+
+	for ( ; env[ptr_i] != NULL; ptr_i++)
+	{
+		for (idx = 0; idx < len_name; idx++)
+		{
+			if (env[ptr_i][idx] == name[idx])
+				validate += 1;
+		}
+		if (validate  == len_name)
+			break;
+		validate = 0;
+	}
+
+	/* duplicate array */
+	mem_pt = _strdup(env[ptr_i]);
+	if (!mem_pt)
+		return (NULL);
+
+	return (mem_pt);
+}
+
+/**
+ * checkCMDpath - check if cmd exists a list of path
+ * belongs to $PATH var
+ * @cmd: pointer to string
+ * @path: points to array  contains "path:path:path"
+ * Return: void
+ */
+
+char *checkCMDpath(char *cmd, char *path)
+{
+	char *full_path;
+	char *token;
+
+	int len_cmd;
+
+	struct stat sh;
+
+	/* check if given command contains path and is valid */
+	if (stat(cmd, &sh) == 0)
+		return (NULL);
+
+
+	/* remove VAR= */
+	token = strtok(path, "=");
+
+	token = strtok(NULL, ":");
+	len_cmd = _strlen(cmd);
+
+	while (token)
+	{
+		/* locate space for current token */
+		full_path = malloc((_strlen(token) + len_cmd) * sizeof(char) + 2);
+		if (!full_path)
+			return (NULL);
+
+		/* copy token to full_path & and add /<cmd> */
+
+		_strncpy(full_path, token, 1);
+
+		if (stat(_strcat(full_path, cmd), &sh) == 0)
+			return (full_path);
+
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
+	return (NULL);
+}
---
+++ main.c
@@ -0,0 +1,162 @@
+#include "shell.h"
+
+#define MAX_BUFSIZE 64
+#define DELIM " \t\r\a\n"
+
+/**
+  * parser - takes the string buffer created by getline and splits it
+  * using strtok. new space will be allocated containg the splited string
+  * and must be freed by the user.
+  *
+  * @buffer: pointer
+  * Return: pointer to the newly allocated memory
+  */
+
+char **parser(char *buffer)
+{
+		char **args = NULL;
+		char *token;
+
+		int bufsize = MAX_BUFSIZE;
+		int c = 0;
+
+		args = malloc(bufsize * sizeof(char *));
+		if (args == NULL)
+		{
+			perror("shell");
+		}
+
+		/* handle first token */
+		token = strtok(buffer, DELIM);
+		if (token == NULL)
+		{
+			perror("shell");
+		}
+
+		while (token != NULL)
+		{
+			args[c++] = token;
+
+			token = strtok(NULL, DELIM);
+		}
+
+		args[c] = NULL;
+		return (args);
+
+}
+
+/**
+ * executer_process - creates a new child process then executes
+ * the give command using execve.
+ *
+ * @array: takes an 2d array
+ * Return: 0 on success OR 1 on fail
+ */
+
+int executer_process(char **array)
+{
+	char *ptr_mem = NULL;
+	char *ptr_path = NULL;
+	pid_t pd, wpid;
+
+	int status;
+
+	pd = fork();
+	if (pd < 0)
+	{
+		perror("shell");
+		return (0);
+	}
+
+	else if (pd ==  0)
+	{
+		/*get $PATH check for command */
+		ptr_path = _getenv("PATH", environ);
+		ptr_mem = checkCMDpath(array[0], ptr_path);
+
+		if (ptr_mem != NULL)
+			array[0] = ptr_mem;
+
+		execve(array[0], array, environ);
+		perror(array[0]);
+		free(ptr_path);
+		return (0);
+	}
+
+	while ((wpid = wait(&status)) > 0)
+	;
+
+	if (ptr_mem != NULL)
+	{
+	array[0] = NULL;
+		free(ptr_mem);
+	}
+	return (1);
+}
+
+/**
+  * prompt - takes input from stdin using getline
+  * Return: pointer
+  */
+
+char *prompt(void)
+{
+	ssize_t ret;
+	size_t size = 0;
+
+	char *buffer = NULL;
+
+	write(STDIN_FILENO, "#> ", 3);
+
+	ret = getline(&buffer, &size, stdin);
+
+	if (ret <= 0)
+	{
+		free(buffer);
+
+		if (isatty(STDIN_FILENO))
+			write(STDIN_FILENO, "\n", 1);
+
+		exit(0);
+	}
+
+	/* -1 to remove the counted new line */
+	if (_stroc(' ', buffer) == _strlen(buffer) - 1)
+	{
+		free(buffer);
+		buffer = NULL;
+		return (prompt());
+	}
+
+	return (buffer);
+}
+
+/**
+ * main - entry point of the shell program
+ * Return: int
+ */
+
+int main(void)
+{
+	int status = 1;
+
+	char *_buffer = NULL;
+	char **ptr;
+
+	while (status)
+	{
+
+		_buffer = prompt();
+
+		ptr = parser(_buffer);
+		status = executer_process(ptr);
+
+		/* clean */
+		free(_buffer);
+		_buffer = NULL;
+		free(ptr);
+
+	}
+
+	return (0);
+}
