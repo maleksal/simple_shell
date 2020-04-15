71e875f3df838b1ab752d272afcd3eb160767439
Malek Salem <malek.salem.14@gmail.com>
2020-04-15 19:48:15 +0000

    fix

--- main.c
+++ main.c
@@ -166,7 +166,7 @@ int main(int ac, char *av[], char **env)
 		{
 			free(ptr);
 			free(_buffer);
-			_exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 		}
 
 		status = executer_process(ptr, &exec_status, av[0], env);
